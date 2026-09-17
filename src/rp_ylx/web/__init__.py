541837facc474c715ed510ac3e1d706c3abcc5e0
sonder <112090565+Alpenl@users.noreply.github.com>
2026-09-17 16:13:48 +0800

    Pin Echo with HEVC playback compatibility guidance

--- src/rp_ylx/web/__init__.py
+++ src/rp_ylx/web/__init__.py
@@ -20,7 +20,7 @@ MANIFEST_SCHEMA_V1 = "openaria.echo-web-artifacts.v1"
 MANIFEST_SCHEMA = "openaria.echo-web-artifacts.v2"
 ENTRY_ASSET = "index.html"
 ECHO_WEB_SOURCE_REPOSITORY = "https://github.com/Alpenl/openaria-echo-web"
-ECHO_WEB_SOURCE_COMMIT = "d6a623e8cd8a16aac459dbcfa066bfadc1357a78"
+ECHO_WEB_SOURCE_COMMIT = "e126a5c33beed15fee946baeb7db1a432398bb50"
 DEVICE_API_VERSION = re.compile(r"^v([1-9][0-9]*)$")
 
 
